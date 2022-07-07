10acce805e1d222f965febedc2ff1f89026691c0
gwanmgks <101333646+gwanmgks@users.noreply.github.com>
2022-07-07 19:14:13 +0900

    Update [SSY]Loops.py

--- [01주차]/[Loops]/[SSY]Loops.py
+++ [01주차]/[Loops]/[SSY]Loops.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
     n = int(input())
-    for i in range(n):
-        result = i*i 
+    for i in range(n):# 직역하면 i가 ~범위에 있는 동안이라는 뜻이며 0<=i<n 의 범위 안에 있는것들을 출력한다.
+        result = i*i # i*i아니면 i**2로 대체가능하다.
         print(result)
