7eb11973e743d2ad96363df71ee0446b6d24304c
gwanmgks <101333646+gwanmgks@users.noreply.github.com>
2022-07-07 18:31:03 +0900

    Update [SSY]Python: Division.py

--- [01주차]/[Python: Division]/[SSY]Python: Division.py
+++ [01주차]/[Python: Division]/[SSY]Python: Division.py
@@ -1,8 +1,8 @@
 from __future__ import division
 
 if __name__ == '__main__':
-    a = int(raw_input())
-    b = int(raw_input())
+    a = int(input())
+    b = int(input())
 
     int_result = int(a/b)
     float_result = a/b
